521c9326106861b744e2bc9bc136bb474c0d1681
NarmeenAli317 <167220056+NarmeenAli317@users.noreply.github.com>
2024-11-03 12:23:21 -0500

    Delete repair-app directory

--- repair-app/backend/routes/commentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/commentController');
-
-router.post('/', commentController.createComment);
-router.get('/:ticketId', commentController.getCommentsByTicketId);
-
-module.exports = router;
